4f67f24a5712298b3d1c6391b5ef0722731f3000
hokoro <hokoro123@gmail.com>
2021-11-25 22:02:41 +0900

    chatservice commit 1 index,room html making

---
+++ chatservice/asgi.py
@@ -0,0 +1,22 @@
+"""
+ASGI config for chatservice project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatservice.settings')
+
+# application = get_asgi_application()
+
+# channels routing setting
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+})
